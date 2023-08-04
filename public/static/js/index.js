c61719a4ca4df58ef3e2d2c85400ee0b2e67772d
xiaofan <fanzh610@163.com>
2023-08-05 01:27:09 +0800

    music --codezujian

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -16,3 +16,42 @@ function customParseLinkUrl(url) {                // JS 语法
     }
     return url
 }
+
+$(function () {
+    //播放完毕
+    $('#mp3Btn').on('ended', function () {
+        console.log("音频已播放完成");
+        $('.mp3Box').css({'background': 'url(/static/image/close.png) no-repeat center bottom', 'background-size': 'cover'});
+    });
+    //播放器控制
+    var audio = document.getElementById('mp3Btn');
+    audio.volume = .3;
+    $('.mp3Box').click(function () {
+        event.stopPropagation();//防止冒泡
+        if (audio.paused) { //如果当前是暂停状态
+            $('.mp3Box').css({'background': 'url(/static/image/open.png) no-repeat center bottom', 'background-size': 'cover'});
+            audio.play(); //播放
+        } else {//当前是播放状态
+            $('.mp3Box').css({
+                'background': 'url(/static/image/close.png) no-repeat center bottom',
+                'background-size': 'cover'
+            });
+            audio.pause(); //暂停
+        }
+    });
+})
+$('#search').click(function () {
+    let keyword = $('#keyword').val();
+    if (keyword == '' || keyword == undefined) {
+        layer.msg('搜索内容不能为空');
+        return;
+    }
+    window.location.replace("/search?keyword=" + keyword);
+})
+var img = $("img");
+img.on("contextmenu", function () {
+    return false;
+});
+img.on("dragstart", function () {
+    return false;
+});
