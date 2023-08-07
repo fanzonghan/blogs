382ed5853397108c1e3e6d029f8e9ce89ac68e65
xiaofan <fanzh610@163.com>
2023-08-07 15:41:50 +0800

    优化

--- app/admin/controller/Index.php
+++ app/admin/controller/Index.php
@@ -1,9 +1,12 @@
 <?php
-declare (strict_types = 1);
+declare (strict_types=1);
 
 namespace app\admin\controller;
 
 use app\admin\AdminController;
+use app\model\Article;
+use app\model\Feedback;
+use app\model\SystemLog;
 
 class Index extends AdminController
 {
@@ -11,10 +14,22 @@ class Index extends AdminController
     {
         return $this->fetch();
     }
-    public function home(){
+
+    public function home()
+    {
+        $ArticleModel = new Article();
+        $articleTotal = $ArticleModel->where('is_del', 0)->count();
+        $FeedbackModel = new Feedback();
+        $feedbackTotal = $FeedbackModel->where('status', 0)->count();
+        $SystemLogModel = new SystemLog();
+        $day_user_beg = $SystemLogModel->where('type', 0)->where('add_time', '>=', strtotime('Ymd'))->count();
+        $day_user_num = $SystemLogModel->where('type', 0)->where('add_time', '>=', strtotime('Ymd'))->group('ip')->count();
+        $this->assign(['articleTotal' => $articleTotal, 'feedbackTotal' => $feedbackTotal, 'day_user_beg' => $day_user_beg,'day_user_num'=>$day_user_num]);
         return $this->fetch();
     }
-    public function clear(){
+
+    public function clear()
+    {
         $this->success('清除成功');
     }
 }
