b94fac5c7864536d645f86c69ab6f9fd0e0fc58c
xiaofan <fanzh610@163.com>
2023-08-19 15:42:29 +0800

    优化

--- app/index/middleware/LogMiddleware.php
+++ app/index/middleware/LogMiddleware.php
@@ -4,6 +4,8 @@
 namespace app\index\middleware;
 
 use app\model\SystemLog;
+use cznet\IpLocation;
+use think\facade\Log;
 
 /**
  * @description: 神兽保佑 永无bug
@@ -16,15 +18,16 @@ class LogMiddleware
     public function handle($request, \Closure $next)
     {
         $SystemLogModel = new SystemLog();
-//        if($request->isAjax()){
-            $SystemLogModel->save([
-                'page' => $request->url(),
-                'type' => 0,
-                'data' => json_encode($request->param(), JSON_UNESCAPED_UNICODE),
-                'ip' => get_user_ip(),
-                'add_time' => time()
-            ]);
-//        }
+        $IpLocation = new IpLocation();
+        $location = $IpLocation->getlocation((string)get_user_ip());
+        $SystemLogModel->save([
+            'page' => $request->url(),
+            'type' => 0,
+            'data' => json_encode($request->param(), JSON_UNESCAPED_UNICODE),
+            'ip' => get_user_ip(),
+            'location' => $location['country'] . '(' . $location['area'] . ')',
+            'add_time' => time()
+        ]);
         return $next($request);
     }
 }
